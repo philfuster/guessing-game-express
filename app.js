4335c39319644d68bcb8c7d55d676b8785c2cd4c
PhilFuster <pafus23@protonmail.com>
2020-04-04 04:12:43 -0400

    Added styles from previous iteration of guessing game. Formatted error page.

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "PAF Guessing Game w/ Express" });
 });
 
 module.exports = router;
