cf02811eb4cb184313ce5cd2da98348839047cb6
PhilFuster <pafus23@protonmail.com>
2020-04-04 06:36:14 -0400

    Added debug package

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const debug = require("debug")("http");
+
+debug.log = console.log.bind(console);
 
 const indexRouter = require("./routes/index");
+const startRouter = require("./routes/start");
 
 const app = express();
 
@@ -18,7 +22,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
+app.use("/start", startRouter);
+// On intialization of app..
+app.on("listening", function () {
+  debug("Application now listening...");
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
