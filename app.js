58add30b6b8c98ba08434da4ca03f78450274be6
PhilFuster <pafus23@protonmail.com>
2020-04-05 01:52:42 -0400

    Modified routing. Added background for guessing game. Implemented Transparent background. Added prettier configuration.

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const debug = require("debug")("http");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const debug = require('debug');
 
+// Binding debug's output to the console.
+// eslint-disable-next-line no-console
 debug.log = console.log.bind(console);
+const log = debug('guess:log');
 
-const indexRouter = require("./routes/index");
-const startRouter = require("./routes/start");
+const indexRouter = require('./routes/index.js');
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(logger("dev"));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/routes')));
 
-app.use("/", indexRouter);
-app.use("/start", startRouter);
-// On intialization of app..
-app.on("listening", function () {
-  debug("Application now listening...");
-});
+app.use('/', indexRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -36,11 +35,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 /* eslint-disable func-names */
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
+const debug = require('debug');
+
+let log = debug('guess:index');
+
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "PAF Guessing Game w/ Express" });
+router.get('/', function (req, res, next) {
+  log('Serving this shit up boi');
+  res.render('index', { title: 'Guessing Game w/ Express' });
+});
+
+/* GET start page. */
+// eslint-disable-next-line no-unused-vars
+router.get('/start', function (req, res, next) {
+  log('We startin baby..');
+  res.render('start', { title: 'PAF Guessing Game' });
 });
 
 module.exports = router;
